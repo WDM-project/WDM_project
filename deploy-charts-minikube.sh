16b0ff877f5589ae7fd0cb0368e4a76e88244d40
ZihaoXu <z.xu@student.utwente.nl>
2023-06-07 00:23:13 +0200

    to build image

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -3,6 +3,10 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+helm uninstall redis
+helm uninstall redis1
+helm uninstall redis2
+
 helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
 helm install -f helm-config/redis-helm-values.yaml redis1 bitnami/redis
 helm install -f helm-config/redis-helm-values.yaml redis2 bitnami/redis
