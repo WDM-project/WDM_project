d0fc1e6336353bceb4a63c209004bf9bfdb506ec
ltcdCai <weitingcai2020@gmail.com>
2023-06-06 15:39:58 +0200

    try6Revert

--- stress-test-k8s/docker-image/locust-tasks/tasks.py
+++ stress-test-k8s/docker-image/locust-tasks/tasks.py
@@ -13,13 +13,9 @@ from locust import HttpUser, SequentialTaskSet, between, task
 #     PAYMENT_URL = urls['PAYMENT_URL']
 #     STOCK_URL = urls['STOCK_URL']
 
-# ORDER_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
-# PAYMENT_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
-# STOCK_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
-
-ORDER_URL = "http://order-service:8000"
-PAYMENT_URL = "http://user-service:8000"
-STOCK_URL = "http://stock-service:8000"
+ORDER_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
+PAYMENT_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
+STOCK_URL = "http://"+os.environ['TARGET_HOST'] + ":8000"
 
 def create_item(session):
     price = random.uniform(1.0, 10.0)
