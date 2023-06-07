b1c07faa18f5a8c8efef3458f6a0d90917245a13
ltcdCai <weitingcai2020@gmail.com>
2023-06-07 16:39:39 +0200

    PartitionTypo

---
+++ deployAll.sh
@@ -0,0 +1,10 @@
+kubectl delete -f k8s/.
+kubectl delete -f stress-test-k8s/kubernetes-config.
+helm uninstall redis
+helm uninstall redis1
+helm uninstall redis2
+helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+helm install -f helm-config/redis-helm-values.yaml redis1 bitnami/redis
+helm install -f helm-config/redis-helm-values.yaml redis2 bitnami/redis
+kubectl apply -f k8s/.
+kubectl apply -f stress-test-k8s/kubernetes-config.
